291f86d1ebfdd7dc8e8c068e66a3882802b97a19
sasidhar <sasidhar.678@gmail.com>
2016-05-16 12:56:30 +0530

    initial commit

---
+++ bug_tracker.js
@@ -0,0 +1,77 @@
+/**
+ * Created by SASi on 10-May-16.
+ */
+
+
+
+var bugTracker = {
+    start: function (apiKey) {
+        var self = this;
+        self.apiKey = apiKey;
+        self.getBrowser = function (regex) {
+            var ua = navigator.userAgent, tem, M = ua.match(regex) || [];
+            if (/trident/i.test(M[1])) {
+                tem = /\brv[ :]+(\d+)/g.exec(ua) || [];
+                return {name: 'IE', version: (tem[1] || '')};
+            }
+            if (M[1] === 'Chrome') {
+                tem = ua.match(/\bOPR\/(\d+)/);
+                if (tem != null) {
+                    return {name: 'Opera', version: tem[1]};
+                }
+                tem = ua.match(/\bedge\/(\d+)/);
+                if (tem != null) {
+                    return {name: 'Edge', version: tem[1]};
+                }
+            }
+            M = M[2] ? [M[1], M[2]] : [navigator.appName, navigator.appVersion, '-?'];
+            if ((tem = ua.match(/version\/(\d+)/i)) != null) {
+                M.splice(1, 1, tem[1]);
+            }
+            return {
+                name: M[0],
+                version: M[1]
+            };
+        };
+
+        self.sendReport = function (report) {
+            report.key = self.apiKey;
+            
+            var url = "<your-host>"; // replace with url where you want to submit your error reports
+            
+            var xhReq = new XMLHttpRequest();
+            xhReq.open("POST", url, false);
+            xhReq.send(report);
+            var serverResponse = xhReq.responseText;
+            console.log(serverResponse); 
+        };
+
+        window.onerror = function (message, source, lineno, colno, errorobj) {
+            var regex = /(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i;
+            var isOpera = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
+            var isFirefox = typeof InstallTrigger !== 'undefined';
+            var isSafari = Object.prototype.toString.call(window.HTMLElement).indexOf('Constructor') > 0;
+            var isIE = /*@cc_on!@*/false || !!document.documentMode;
+            var isEdge = !isIE && !!window.StyleMedia;
+            if (isEdge) regex = /(edge(?=\/))\/?\s*(\d+)/i;
+            var isChrome = !!window.chrome && !!window.chrome.webstore;
+            var isBlink = (isChrome || isOpera) && !!window.CSS;
+
+            var browser = self.getBrowser(regex);
+            var msg = message.split(':');
+            var errorReport = {
+                "error_type": msg[0],
+                "error_message": msg[1],
+                "url": source,
+                "line_number": lineno,
+                "column_number": colno,
+                "stack_trace": errorobj.stack,
+                "browser_details": browser.name + "(V-" + browser.version + ")",
+                "operating_system": navigator.platform,
+                "time_stamp": new Date().getTime()
+            };
+            self.sendReport(errorReport);
+        }
+    }
+};
+
